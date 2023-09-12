5522051b863dd15088131d61470c741a81e0b4db
AltESV <ishigami.daniel@gmail.com>
2023-09-12 11:21:58 +0100

    updated handler

--- server.js
+++ server.js
@@ -6,13 +6,21 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', async (req, res) => {
+    try {
+    
     const data = req.body;
 
     // Add forwarding function (e.g., hash the email, forward to Facebook)
 
-    res.json({ status: 'success' });
+    res.status(200).json({ status: 'success' });
     console.log(req)
+        } catch(err) {
+            res.status(400).json({
+                status: 'fail',
+                message: err
+            })
+        }
 });
 
 app.listen(port, () => {
