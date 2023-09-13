765b8d564ce0226796c007dda1dce585672158a9
AltESV <ishigami.daniel@gmail.com>
2023-09-13 14:40:56 +0100

    implementing conversions api

---
+++ README.md
@@ -0,0 +1,10 @@
+Event detail parameters:
+
+action_source needs to be 'website' 
+event_name can be custom 'signup'
+event_source_url is 'https://www.fanaverse.io/'
+event_time is 1694609135 as unix timestamp int
+
+Customer information parameters
+
+Email address as hash
--- server.js
+++ server.js
@@ -1,20 +1,65 @@
+//IMPORTS
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config('')
+const bizSdk = require('facebook-nodejs-business-sdk');
+const access_token = process.env.ACCESS_TOKEN;
+const pixel_id = process.env.ADS_PIXEL_ID;
+const EventRequest = bizSdk.EventRequest;
+const UserData = bizSdk.UserData;
+const ServerEvent = bizSdk.ServerEvent;
+const api = bizSdk.FacebookAdsApi.init(access_token);
 
+//INITIALIZE
 const app = express();
 const port = process.env.PORT || 8080;
 
+//WEBHOOK
 app.use(bodyParser.json());
 
 app.post('/webhook', async (req, res) => {
     try {
-    
-    const data = req.body;
+        if (!req.body.user.email) {
+            return res.status(400).json({
+                status: 'fail',
+                message: 'no email received'
+            })
+        }
+        const email = req.body.user.email
+        
+        //FORWARD EVENT TO FACEBOOK
+        let current_timestamp = Math.floor(new Date() / 1000);
 
-    // Add forwarding function (e.g., hash the email, forward to Facebook)
+        const userData = (new UserData())
+                        .setEmails([email])
 
-    res.status(200).json({ status: 'success' });
-    console.log(req)
+        const serverEvent = (new ServerEvent())
+                        .setEventName('signup')
+                        .setEventTime(current_timestamp)
+                        .setUserData(userData)
+                        .setEventSourceUrl('https://www.fanaverse.io/')
+                        .setActionSource('website');
+
+        const eventsData = [serverEvent];
+        const eventRequest = (new EventRequest(access_token, pixel_id))
+                        .setEvents(eventsData);
+
+        eventRequest.execute().then(
+        response => {
+                console.log('Facebook Response: ', response);
+                res.status(200).json({
+                    status: 'success'
+                })
+        },
+        err => {
+            console.error('Error: ', err);
+            res.status(500).json({
+                status: 'fail',
+                message: 'error sending to facebook'
+            })
+        }
+        );
+
         } catch(err) {
             res.status(400).json({
                 status: 'fail',
@@ -23,6 +68,7 @@ app.post('/webhook', async (req, res) => {
         }
 });
 
+//SERVER
 app.listen(port, () => {
     console.log(`Webhook server is running on port ${port}`);
 });
