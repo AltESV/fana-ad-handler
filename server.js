32c9cf740d20701398b58e1e9427d20732e1d39f
AltESV <ishigami.daniel@gmail.com>
2023-09-22 11:29:17 +0100

    updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //IMPORTS
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config('')
-const bizSdk = require('facebook-nodejs-business-sdk');
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config("");
+const bizSdk = require("facebook-nodejs-business-sdk");
 const access_token = process.env.ACCESS_TOKEN;
 const pixel_id = process.env.ADS_PIXEL_ID;
 const EventRequest = bizSdk.EventRequest;
@@ -17,58 +17,48 @@ const port = process.env.PORT || 8080;
 //WEBHOOK
 app.use(bodyParser.json());
 
-app.post('/webhook', async (req, res) => {
-    try {
-        if (!req.body.user.email) {
-            return res.status(400).json({
-                status: 'fail',
-                message: 'no email received'
-            })
-        }
-        const email = req.body.user.email
-        
-        //FORWARD EVENT TO FACEBOOK
-        let current_timestamp = Math.floor(new Date() / 1000);
+app.post("/webhook", async (req, res) => {
+  try {
+    if (!req.body.user.email) {
+      return res.status(400).json({
+        status: "fail",
+        message: "no email received",
+      });
+    }
+    const email = req.body.user.email;
 
-        const userData = (new UserData())
-                        .setEmails([email])
+//FORWARD EVENT TO FACEBOOK
+    let current_timestamp = Math.floor(new Date() / 1000);
 
-        const serverEvent = (new ServerEvent())
-                        .setEventName('signup')
-                        .setEventTime(current_timestamp)
-                        .setUserData(userData)
-                        .setEventSourceUrl('https://www.fanaverse.io/')
-                        .setActionSource('website');
+    const userData = new UserData().setEmails([email]);
 
-        const eventsData = [serverEvent];
-        const eventRequest = (new EventRequest(access_token, pixel_id))
-                        .setEvents(eventsData);
+    const serverEvent = new ServerEvent()
+      .setEventName("signup")
+      .setEventTime(current_timestamp)
+      .setUserData(userData)
+      .setEventSourceUrl("https://www.fanaverse.io/")
+      .setActionSource("website");
 
-        eventRequest.execute().then(
-        response => {
-                console.log('Facebook Response: ', response);
-                res.status(200).json({
-                    status: 'success'
-                })
-        },
-        err => {
-            console.error('Error: ', err);
-            res.status(500).json({
-                status: 'fail',
-                message: 'error sending to facebook'
-            })
-        }
-        );
+    const eventsData = [serverEvent];
+    const eventRequest = new EventRequest(access_token, pixel_id).setEvents(
+      eventsData,
+    );
 
-        } catch(err) {
-            res.status(400).json({
-                status: 'fail',
-                message: err
-            })
-        }
+    const response = await eventRequest.execute();
+
+    console.log("Facebook Response: ", response);
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    });
+  }
 });
 
 //SERVER
 app.listen(port, () => {
-    console.log(`Webhook server is running on port ${port}`);
+  console.log(`Webhook server is running on port ${port}`);
 });
